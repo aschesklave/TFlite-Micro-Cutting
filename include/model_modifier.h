193e065e21961e920370fee94509273d33f0e0fa
Christopher Hinterer <hinterer@student.tugraz.at>
2023-02-06 19:04:30 +0100

    Updated model modifier

--- include/model_modifier.h
+++ include/model_modifier.h
@@ -22,10 +22,11 @@ class ModelModifier
   public:
     void modifyFullyConnectedShape(const int32_t layer_index, const int32_t new_shape);
     void modify2DConvolutionalShape(const int32_t layer_index, const int32_t new_shape);
+    int32_t getMultipliedTensorShape(const int32_t tensor_index);
     int32_t getInputTensorIndex(const uint32_t& target_op_index);
     int32_t getOutputTensorIndex(const uint32_t& target_op_index);
     int32_t getWeightTensorIndex(const uint32_t& target_op_index);
-    int32_t setTensorShape(const int32_t tensor_index, const int32_t new_shape, const uint32_t shape_index);
+    int8_t setTensorShape(const int32_t tensor_index, const int32_t new_shape, const uint32_t shape_index, int32_t &shape_diff);
 
     ModelModifier(tflite::Model* model);
     ~ModelModifier();
--- src/model_modifier.cpp
+++ src/model_modifier.cpp
@@ -68,19 +68,19 @@ int32_t ModelModifier::getWeightTensorIndex(const uint32_t& target_op_index) {
   return (*target_op->inputs())[1];
 }
 
-int32_t ModelModifier::setTensorShape(const int32_t tensor_index, const int32_t new_shape, const uint32_t shape_index) {
+int8_t ModelModifier::setTensorShape(const int32_t tensor_index, const int32_t new_shape, const uint32_t shape_index, int32_t &shape_diff) {
   const tflite::SubGraph* subgraph = (*model_->subgraphs())[0];
   if (tensor_index < 0 || (uint32_t)tensor_index >= subgraph->tensors()->size()) {
     MicroPrintf("ERROR: tensor index out of bounds.");
     Serial1.println("ERROR: tensor index out of bounds.");
-    return -1;
+    return 1;
   }
   const tflite::Tensor* tensor = (*subgraph->tensors())[tensor_index];
   const flatbuffers::Vector<int32_t>* shape_vector_const = tensor->shape();
   if (shape_index >= shape_vector_const->size()) {
     MicroPrintf("ERROR: shape index out of bounds.");
     Serial1.println("ERROR: shape index out of bounds.");
-    return -1;
+    return 1;
   }
   const int32_t old_shape = (*shape_vector_const)[shape_index];
   if (new_shape != old_shape) {
@@ -93,14 +93,27 @@ int32_t ModelModifier::setTensorShape(const int32_t tensor_index, const int32_t 
     if(new_shape != (*shape_vector)[shape_index]) {
       MicroPrintf("ERROR: Shape change failed.");
       Serial1.println("ERROR: Shape change failed.");
-      return -1;
+      return 1;
     }
   }
-
-  if (shape_index == 1) {
-    return old_shape - new_shape;
-  }
+  shape_diff = old_shape - new_shape;
   return 0;
+}
+
+int32_t ModelModifier::getMultipliedTensorShape(const int32_t tensor_index) {
+  const tflite::SubGraph* subgraph = (*model_->subgraphs())[0];
+  if((uint32_t)tensor_index >= subgraph->tensors()->size()) {
+    MicroPrintf("ERROR: tensor index out of bounds.");
+    Serial1.println("ERROR: tensor index out of bounds.");
+    return -1;
+  }
+  const tflite::Tensor* tensor = (*subgraph->tensors())[tensor_index];
+  const flatbuffers::Vector<int32_t>* shape_vector = tensor->shape();
+  int32_t result = 1;
+  for(uint8_t i = 0; i < shape_vector->size(); ++i) {
+    result *= (*shape_vector)[i];
+  }
+  return result;
 }
 
 void ModelModifier::modifyFullyConnectedShape(const int32_t layer_index, const int32_t new_shape) {
@@ -108,8 +121,12 @@ void ModelModifier::modifyFullyConnectedShape(const int32_t layer_index, const i
     MicroPrintf("ERROR: No FULLY_CONNECTED layer found.");
     Serial1.println("ERROR: No FULLY_CONNECTED layer found.");
   }
-  //TODO: Check if layer is out of bounds!
   const tflite::SubGraph* subgraph = (*model_->subgraphs())[0];
+  if(layer_index <= 0 || (uint32_t)layer_index >= subgraph->operators()->size()) {
+    MicroPrintf("ERROR: layer index out of bounds.");
+    Serial1.println("ERROR: layer index out of bounds.");
+    return;
+  }
   const tflite::Operator* target_op = (*subgraph->operators())[layer_index];
   if(target_op->opcode_index() != op_index_fully_connected_) {
     MicroPrintf("ERROR: Layer to modify is not Fully-Connected.");
@@ -121,11 +138,14 @@ void ModelModifier::modifyFullyConnectedShape(const int32_t layer_index, const i
   if(target_tensor < 0 || next_target_tensor < 0) {
     return;
   }
-  int32_t res = setTensorShape(target_tensor, new_shape, 0);
-  if(res < 0) return;
-  res = setTensorShape(next_target_tensor, new_shape, 1);
-  if(res < 0) return;
-  weight_offset[layer_index + 1] += res;
+  int32_t shape_diff;
+  int8_t status = setTensorShape(target_tensor, new_shape, 0, shape_diff);
+  if(0 == status) {
+    status = setTensorShape(next_target_tensor, new_shape, 1, shape_diff);
+  }
+  if(0 == status) {
+    weight_offset[layer_index + 1] += shape_diff;
+  }
 }
 
 void ModelModifier::modify2DConvolutionalShape(const int32_t layer_index, const int32_t new_shape) {
@@ -134,6 +154,11 @@ void ModelModifier::modify2DConvolutionalShape(const int32_t layer_index, const 
     Serial1.println("ERROR: No CONV_2D layer found.");
   }
   const tflite::SubGraph* subgraph = (*model_->subgraphs())[0];
+  if(layer_index <= 0 || (uint32_t)layer_index >= subgraph->operators()->size()) {
+    MicroPrintf("ERROR: layer index out of bounds.");
+    Serial1.println("ERROR: layer index out of bounds.");
+    return;
+  }
   const tflite::Operator* target_op = (*subgraph->operators())[layer_index];
   if(target_op->opcode_index() != op_index_fully_connected_) {
     MicroPrintf("ERROR: Layer to modify is not Conv2D.");
@@ -145,26 +170,45 @@ void ModelModifier::modify2DConvolutionalShape(const int32_t layer_index, const 
   int32_t first_output_tensor = getOutputTensorIndex(layer_index);
   int32_t first_pool_output_tensor = getOutputTensorIndex(layer_index + 1);
 
-  if(setTensorShape(first_weight_tensor, new_shape, 0) < 0) return;
-  if(setTensorShape(first_output_tensor, new_shape, 3) < 0) return;
-  if(setTensorShape(first_pool_output_tensor, new_shape, 3) < 0) return;
+  int32_t shape_diff;
+  int8_t status = setTensorShape(first_weight_tensor, new_shape, 0, shape_diff);
+
+  if (0 == status) {
+    status = setTensorShape(first_output_tensor, new_shape, 3, shape_diff);
+  }
+
+  if (0 == status) {
+    status = setTensorShape(first_pool_output_tensor, new_shape, 3, shape_diff);
+  }
 
   int32_t second_layer_index = layer_index + 2;
   int32_t second_weight_tensor = getWeightTensorIndex(second_layer_index);
   int32_t second_output_tensor = getOutputTensorIndex(second_layer_index);
   int32_t second_pool_output_tensor = getOutputTensorIndex(second_layer_index + 1);
 
-  if(setTensorShape(second_weight_tensor, new_shape, 0) < 0) return;
-  int32_t res = setTensorShape(second_weight_tensor, new_shape, 3);
-  if(res < 0) return;
-  weight_offset[second_layer_index] += 1;
-  if(setTensorShape(second_output_tensor, new_shape, 3) < 0) return;
-  if(setTensorShape(second_pool_output_tensor, new_shape, 3) < 0) return;
+  if (0 == status) {
+    status = setTensorShape(second_weight_tensor, new_shape, 0, shape_diff);
+  }
+  if (0 == status) {
+    status = setTensorShape(second_weight_tensor, new_shape, 3, shape_diff);
+  }
+  if (0 == status) {
+    weight_offset[second_layer_index] += shape_diff;
+    status = setTensorShape(second_output_tensor, new_shape, 3, shape_diff);
+  }
+  if (0 == status) {
+    status = setTensorShape(second_pool_output_tensor, new_shape, 3, shape_diff);
+  }
 
+  int32_t fc_shape = getMultipliedTensorShape(second_output_tensor);
+
+  // TODO: Get dense layer index
   int32_t fc_layer_idx = 5;
   int32_t fc_weight_tensor = getWeightTensorIndex(fc_layer_idx);
-  res = setTensorShape(fc_weight_tensor, 49, 1);
-  if(res < 0) return;
-  weight_offset[fc_layer_idx] += res;
-
+  if (0 == status) {
+    status = setTensorShape(fc_weight_tensor, fc_shape, 1, shape_diff);
+  }
+  if (0 == status) {
+    weight_offset[fc_layer_idx] += shape_diff;
+  }
 }
